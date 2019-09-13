676e702b08597dbbdceb92a76ee55d2d55890d7c
Adam Wójs <adam@wojs.pl>
2019-09-13 09:51:12 +0200

    Fixed CS in skeleton/{choice-field-type, entity-field-type}

--- src/bundle/Resources/skeleton/choice-field-type/services/custom.tpl.php
+++ src/bundle/Resources/skeleton/choice-field-type/services/custom.tpl.php
@@ -4,36 +4,36 @@ services:
         autoconfigure: true
         public: false
 
-    <?= $field_type_definition ?>:
-        class: <?= $field_type_definition_class ."\n" ?>
+    <?= $field_type_definition; ?>:
+        class: <?= $field_type_definition_class . "\n"; ?>
         arguments:
-            $choiceProvider: '@<?= $field_type_definition ?>.choice_provider'
+            $choiceProvider: '@<?= $field_type_definition; ?>.choice_provider'
         tags:
-            - { name: ezplatform.field_type, alias: <?= $field_type_identifier ?> }
+            - { name: ezplatform.field_type, alias: <?= $field_type_identifier; ?> }
 
-    <?= $field_type_definition ?>.choice_provider:
-        class: <?= $choice_provider_class . "\n" ?>
+    <?= $field_type_definition; ?>.choice_provider:
+        class: <?= $choice_provider_class . "\n"; ?>
 
-    <?= $field_type_definition ?>.converter:
+    <?= $field_type_definition; ?>.converter:
         class: AdamWojs\EzPlatformFieldTypeLibrary\Core\Persistence\Legacy\Converter\ChoiceConverter
         tags:
-            - { name: ezplatform.field_type.legacy_storage.converter, alias: <?= $field_type_identifier ?> }
+            - { name: ezplatform.field_type.legacy_storage.converter, alias: <?= $field_type_identifier; ?> }
 
-    <?= $field_type_definition ?>.indexable:
+    <?= $field_type_definition; ?>.indexable:
         class: AdamWojs\EzPlatformFieldTypeLibrary\Core\FieldType\AbstractChoice\SearchField
         tags:
-            - { name: ezplatform.field_type.indexable, alias: <?= $field_type_identifier ?> }
+            - { name: ezplatform.field_type.indexable, alias: <?= $field_type_identifier; ?> }
 
-    <?= $field_type_definition ?>.form_mapper.value:
+    <?= $field_type_definition; ?>.form_mapper.value:
         class: AdamWojs\EzPlatformFieldTypeLibrary\Core\FieldType\AbstractChoice\FormMapper\FieldValueFormMapper
         arguments:
-            $choiceProvider: '@<?= $field_type_definition ?>.choice_provider'
+            $choiceProvider: '@<?= $field_type_definition; ?>.choice_provider'
         tags:
-            - { name: ezplatform.field_type.form_mapper.value, fieldType: <?= $field_type_identifier ?> }
+            - { name: ezplatform.field_type.form_mapper.value, fieldType: <?= $field_type_identifier; ?> }
 
-    <?= $field_type_definition ?>.form_mapper.definition:
+    <?= $field_type_definition; ?>.form_mapper.definition:
         class: AdamWojs\EzPlatformFieldTypeLibrary\Core\FieldType\AbstractChoice\FormMapper\FieldDefinitionFormMapper
         tags:
-            - { name: ezplatform.field_type.form_mapper.definition, fieldType: <?= $field_type_identifier ?> }
+            - { name: ezplatform.field_type.form_mapper.definition, fieldType: <?= $field_type_identifier; ?> }
 
 
--- src/bundle/Resources/skeleton/entity-field-type/class/Type.tpl.php
+++ src/bundle/Resources/skeleton/entity-field-type/class/Type.tpl.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace <?= $namespace; ?>;
 
 use AdamWojs\EzPlatformFieldTypeLibrary\Core\FieldType\AbstractEntity\Type as AbstractEntityType;
-use <?= $entity_full_name ?>;
+use <?= $entity_full_name; ?>;
 
 final class Type extends AbstractEntityType
 {
@@ -16,6 +16,6 @@ final class Type extends AbstractEntityType
 
     protected function getValueClass(): string
     {
-        return <?= $entity_short_name ?>::class;
+        return <?= $entity_short_name; ?>::class;
     }
 }
